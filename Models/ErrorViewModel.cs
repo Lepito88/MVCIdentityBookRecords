d3cbdf37085e8b216f9e0925eb3f08b0da624d82
Toni Lepistö <lepito88@gmail.com>
2022-10-12 09:29:24 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCIdentityBookRecords.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
