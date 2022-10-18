665ab172ef6b677f1644ab61a4b992ecd97d6eee
Toni Lepistö <lepito88@gmail.com>
2022-10-18 23:07:53 +0300

    Added Authorization possibility to Swagger

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MVCIdentityBookRecords.Data;
 using MVCIdentityBookRecords.Interfaces;
 using MVCIdentityBookRecords.Models;
@@ -16,6 +17,33 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "MVCIdentityBookRecords", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+}); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 
